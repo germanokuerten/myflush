36325dbef241a2716d7e92c2aa2e2be4475c49a8
Germano Kuerten <germanokuerten@gmail.com>
2022-07-27 00:05:50 -0700

    list of flushes test

--- main_app/views.py
+++ main_app/views.py
@@ -9,4 +9,20 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-    
+def flushes_index(request):
+    return render(request, 'flushes/index.html', {'flushes': flushes})
+
+# Faux Flush Data - Database simulation
+class Flush:
+    def __init__(self, name, author, description, price):
+        self.name = name
+        self.author = author
+        self.description = description
+        self.price = price
+
+# Book objects that are being instantiated from the Book class.
+flushes = [
+    Flush('Rich Dad Poor Dad', 'Robert T. Kiyosaki', ' What the Rich Teach Their Kids About Money That the Poor and Middle Class Do Not!', 7),
+    Flush('Think and Grow Rich', 'Napoleon Hill', '"Think and Grow Rich" is a motivational personal development and self-help book written by Napoleon Hill and inspired by a suggestion from Scottish-American businessman Andrew Carnegie."', 15),
+    Flush('The Alchemist', 'Paulo Coelho', 'A Fable About Following Your Dream', 14),
+]
